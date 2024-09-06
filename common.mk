d2c5c83c0f0bad08019ec157010d011e27c6805f
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-07 02:54:20 +0800

    sm8550-common: overlay: Allow switching between 60Hz and 120Hz
    * At the same time, move the device-specific refreshrate overlays to FrameworksResSamsung.
    
    Change-Id: I4f328dbdce50e709f30b1f098e71248d72819268

--- common.mk
+++ common.mk
@@ -320,6 +320,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResSamsung \
     FrameworksResTarget \
     SettingsResSamsung \
     SystemUIResCommon \
