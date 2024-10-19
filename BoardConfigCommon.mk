bbe05cffef004c81f5474d6a43df4a64434666a7
Mezaque <mezaquegit@gmail.com>
2024-10-19 20:25:42 -0300

    sm8550-common: Back to permissive SElinux
    Change-Id: Ib64effbf01eb65f4ebd755f785d2e9de7085c474

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,7 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
+    androidboot.selinux=permissive \
     androidboot.usbcontroller=a600000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
